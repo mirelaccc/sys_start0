6892b5f235b723bc47539bdbb7df468e072ae218
mirelaccc <30347906+mirelaccc@users.noreply.github.com>
2017-07-26 12:27:40 +0200

    Update paks.R

--- paks.R
+++ paks.R
@@ -1,49 +1,28 @@
-# start0
-
-# inbash:
-> sudo apt-get install libcurl4-openssl-dev libssl-dev
-
-install.packages("devtools")
-library(devtools)
-install.packages("githubinstall")
-library(githubinstall)
-
-
-# if needed:
-# .libPaths( c( .libPaths(), "~/a/b/c") )
 
 # basics from github
 gh_install_packages("mapmate")
 gh_install_packages("RBioFabric")
-gh_install_packages("packages")
 
-library(packages)
+library(mapmate)
+library(RBioFabric)
 
-# listing packages per theme
-my_paks_s0 <- c("dplyr", "RMySQL", "data.table", "RJSONIO", "gdata", "varhandle", "stats", "utils")
+# package blocks
+# grouping packages per work-type
+
+my_paks_s0 <- c("dplyr", "RMySQL", "data.table", "RJSONIO", "gdata", "varhandle", "stats", "utils", "methods", "RCurl " )
+packages(my_paks_s0)
+
 my_paks_unstr_data <- c("RJSONIO", "XML") 
+packages(my_paks_unstr_data)
+
 my_paks_viz <- c("ggplot2", "grDevices", "scales", "colorspace", "RColorBrewer", "grid", "graphics")
+packages(my_paks_viz)
+
 my_paks_net <- c("RBioFabric", "HiveR", "igraph", "visNetwork", "ndtv")
+packages(my_paks_net)
+
 my_paks_mob <- c("tmaptools", "tmap", "OpenStreetMap" , "OSMscale" , "ggmap", "mapmate" , "mapproj"
                  , "mapdata", "maps", "maptools", "mapproj", "mapdata", "maps", "sp", "XML", "plotKML"
-                 , "raster", "trajectories", "geosphere", "rgdal", "classInt")
-
-
-,"","","","","","","","","","","","","" 
-
-
-
-library()
-# loading packages
-libraries(my_paks_s0)
-libraries(my_paks_viz)
-libraries(my_paks_mob)
-
-
-# Tips:
-
-# search paks which have the same name
-#gh_install_packages("some_pak")
-#gh_list_packages("lasso")
-#gh_search_packages("lasso")
+                 , "raster", "trajectories", "geosphere", "rgdal", "rgeos", "leaflet", "spatstat", "gstat", "classInt")
+packages(my_paks_mob)
 
