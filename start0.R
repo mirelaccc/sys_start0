08f09479eabac1d7b5116f76267fc06ad23a2477
mirelaccc <30347906+mirelaccc@users.noreply.github.com>
2017-07-26 11:26:18 +0200

    Update start0.R

--- start0.R
+++ start0.R
@@ -1,6 +1,9 @@
  
 # start0
 
+# if needed:
+# .libPaths( c( .libPaths(), "~/a/b/c") )
+
 # in bash:
 #> sudo apt-get install libcurl4-openssl-dev libssl-dev
 
@@ -8,6 +11,7 @@ install.packages("devtools")
 library(devtools)
 install.packages("githubinstall")
 library(githubinstall)
+# to enable easy package installing & loading:
 gh_install_packages("packages")
 library(packages)
- 
+
